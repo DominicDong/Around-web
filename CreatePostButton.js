3a365163359c5df5e452d621b55486219bbc6c55
Dominic Dong <dominiclt18i@hotmail.com>
2019-01-16 11:43:42 -0500

    Integrate Google Map API

--- CreatePostButton.js
+++ CreatePostButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 import $ from 'jquery';
 import { Modal, Button, message } from 'antd';
 import { WrappedCreatePostForm } from './CreatePostForm';
-import { API_ROOT, POS_KEY, AUTH_PREFIX, TOKEN_KEY } from '../constants';
+import { API_ROOT, POS_KEY, AUTH_PREFIX, TOKEN_KEY, LOC_SHAKE } from '../constants';
 
 export class CreatePostButton extends React.Component {
  state = {
@@ -20,8 +20,8 @@ export class CreatePostButton extends React.Component {
      if (!err) {
        const { lat, lon } = JSON.parse(localStorage.getItem(POS_KEY));
        const formData = new FormData();
-       formData.set('lat', lat);
-       formData.set('lon', lon);
+       formData.set('lat', lat + Math.random() * LOC_SHAKE * 2 - LOC_SHAKE);
+       formData.set('lon', lon + Math.random() * LOC_SHAKE * 2 - LOC_SHAKE);
        formData.set('message', values.message);
        formData.set('image', values.image[0].originFileObj);
 
@@ -39,40 +39,40 @@ export class CreatePostButton extends React.Component {
          message.success('Created a post successfully!');
          this.form.resetFields();
          this.setState({ visible: false, confirmLoading: false });
-         this.props.loadNearByPosts();
-         }, (response) => {
-           message.error(response.responseText);
-           this.setState({ visible: false, confirmLoading: false });
-         }).catch((error) => {
-           console.log(error);
-         });
-       }
-     });
-   }
-   handleCancel = () => {
-     console.log('Clicked cancel button');
-     this.setState({
-       visible: false,
-     });
-   }
-   saveFormRef = (form) => {
-     this.form = form;
-   }
-   render() {
-     const { visible, confirmLoading } = this.state;
-     return (
-       <div>
-         <Button type="primary" onClick={this.showModal}>Create New Post</Button>
-         <Modal title="Create New Post"
-                visible={visible}
-                onOk={this.handleOk}
-                okText="Create"
-                confirmLoading={confirmLoading}
-                onCancel={this.handleCancel}
-         >
-           <WrappedCreatePostForm ref={this.saveFormRef}/>
-         </Modal>
-       </div>
-     );
-   }
-  }
+         this.props.loadNearbyPosts();
+       }, (response) => {
+         message.error(response.responseText);
+         this.setState({ visible: false, confirmLoading: false });
+       }).catch((error) => {
+         console.log(error);
+       });
+     }
+   });
+ }
+ handleCancel = () => {
+   console.log('Clicked cancel button');
+   this.setState({
+     visible: false,
+   });
+ }
+ saveFormRef = (form) => {
+   this.form = form;
+ }
+ render() {
+   const { visible, confirmLoading } = this.state;
+   return (
+     <div>
+       <Button type="primary" onClick={this.showModal}>Create New Post</Button>
+       <Modal title="Create New Post"
+              visible={visible}
+              onOk={this.handleOk}
+              okText="Create"
+              confirmLoading={confirmLoading}
+              onCancel={this.handleCancel}
+       >
+         <WrappedCreatePostForm ref={this.saveFormRef}/>
+       </Modal>
+     </div>
+   );
+ }
+}
